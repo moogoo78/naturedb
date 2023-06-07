8e6c53ccb9a2a9508624afc28cec446ecdaf8d87
MooGoo <moogoo78@gmail.com>
2023-06-07 18:32:03 +0800

    feat: base.portal_search

--- app/blueprints/base.py
+++ app/blueprints/base.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 import re
 import os
+import math
 
 from flask import (
     Blueprint,
@@ -24,9 +25,40 @@ from flask_login import (
 from werkzeug.security import (
     check_password_hash,
 )
-from app.models.site import(
+from sqlalchemy import (
+    select,
+    func,
+    text,
+    desc,
+    cast,
+    between,
+    extract,
+    or_,
+    join,
+)
+from sqlalchemy.orm import (
+    aliased,
+)
+
+from app.models.site import (
     Organization,
     User,
+)
+from app.models.collection import (
+    Collection,
+    Record,
+    RecordAssertion,
+    AssertionType,
+    AreaClass,
+    NamedArea,
+    Unit,
+    UnitAssertion,
+    Identification,
+    Person,
+    Taxon,
+)
+from app.models.taxon import (
+    Taxon,
 )
 from app.helpers import (
     get_current_site,
@@ -34,7 +66,9 @@ from app.helpers import (
 from app.utils import (
     get_domain,
 )
-
+from app.database import (
+    session,
+)
 
 base = Blueprint('base', __name__)
 
@@ -42,6 +76,93 @@ base = Blueprint('base', __name__)
 def portal_list():
     site_list = Organization.query.filter(Organization.is_site==True).all()
     return render_template('portal-list.html', site_list=site_list)
+
+@base.route('/search')
+def portal_search():
+    site_list = Organization.query.filter(Organization.is_site==True).all()
+
+    current_page = int(request.args.get('page', 1))
+    q = request.args.get('q', '')
+
+    taxon_family = aliased(Taxon)
+    stmt = select(Unit.id, Unit.accession_number, Record.id, Record.collector_id, Record.field_number, Record.collect_date, Record.proxy_taxon_scientific_name, Record.proxy_taxon_common_name, Record.proxy_taxon_id) \
+        .join(Unit, Unit.record_id==Record.id) \
+        .join(taxon_family, taxon_family.id==Record.proxy_taxon_id, isouter=True) \
+    #print(stmt, flush=True)
+    if q:
+        stmt = select(Unit.id, Unit.accession_number, Record.id, Record.collector_id, Record.field_number, Record.collect_date, Record.proxy_taxon_scientific_name, Record.proxy_taxon_common_name, Record.proxy_taxon_id) \
+        .join(Unit, Unit.record_id==Record.id) \
+        .join(Person, Record.collector_id==Person.id, isouter=True)
+
+        stmt = stmt.filter(or_(Unit.accession_number.ilike(f'%{q}%'),
+                               Record.field_number.ilike(f'%{q}%'),
+                               Person.full_name.ilike(f'%{q}%'),
+                               Person.full_name_en.ilike(f'%{q}%'),
+                               Record.proxy_taxon_scientific_name.ilike(f'%{q}%'),
+                               Record.proxy_taxon_common_name.ilike(f'%{q}%'),
+                               ))
+
+    # apply collection filter by site
+    #stmt = stmt.filter(Record.collection_id.in_(site.collection_ids))
+    base_stmt = stmt
+    subquery = base_stmt.subquery()
+    count_stmt = select(func.count()).select_from(subquery)
+    total = session.execute(count_stmt).scalar()
+
+    # order & limit
+    stmt = stmt.order_by(desc(Record.id))
+    if current_page > 1:
+        stmt = stmt.offset((current_page-1) * 20)
+    stmt = stmt.limit(20)
+
+    result = session.execute(stmt)
+    rows = result.all()
+    last_page = math.ceil(total / 20)
+    pagination = {
+        'current_page': current_page,
+        'last_page': last_page,
+        'start_to': min(last_page-1, 3),
+        'has_next': True if current_page < last_page else False,
+        'has_prev': True if current_page > 1 else False,
+    }
+    items = []
+    for r in rows:
+        record = session.get(Record, r[2])
+        loc_list = [x.display_name for x in record.named_areas]
+        if loc_text := record.locality_text:
+            loc_list.append(loc_text)
+        collector = ''
+        if r[3]:
+            collector = record.collector.display_name
+        #collector = '{} ({})'.format(r[4], r[5])
+        #elif r[4]:
+        #    collector = r[4]
+
+        entity_id = f'u{r[0]}' if r[0] else f'r{r[2]}'
+
+        # HACK
+        if r[8]:
+            taxon_display = ''
+            if taxon := session.get(Taxon, r[8]):
+                #if family := taxon.get_higher_taxon('family'):
+                #    taxon_family = f
+                taxon_display = taxon.display_name
+
+            item = {
+                'accession_number': r[1] or '',
+                'record_id': r[2],
+                'field_number': r[4] or '',
+                'collector': collector,
+                'collect_date': r[5].strftime('%Y-%m-%d') if r[5] else '',
+                'scientific_name': taxon_display, # r[6]
+                'common_name': '', #r[7],
+                'locality': ','.join(loc_list),
+                'entity_id': entity_id,
+            }
+            items.append(item)
+
+
+    return render_template('portal-search.html', site_list=site_list, items=items, total=total, pagination=pagination)
 
 @base.route('/login', methods=['GET', 'POST'])
 def login():
