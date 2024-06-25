fcd6c81b1be8f81f95dd9230d04e9d03d555dad0
MooGoo <moogoo78@gmail.com>
2024-06-25 14:43:30 +0800

    add taibol certbot

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ up:
 	sudo docker compose up
 down:
 	sudo docker compose down
+
+deploy-sites:
+	scp -r app/static/sites ndb:~/naturedb/app/static
+	scp -r app/templates/sites ndb:~/naturedb/app/templates
--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(www.hast.biodiv.tw)
+domains=(taibol.naturedb.org)
 rsa_key_size=4096
 data_path="../naturedb-volumes/certbot"
 email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml run --rm --entrypoint "\
+sudo docker compose -f compose.yml -f compose.prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +39,11 @@ echo
 
 
 echo "### Starting nginx ..."
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate -d nginx
+sudo docker compose -f compose.yml -f compose.prod.yml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml run --rm --entrypoint "\
+sudo docker compose -f compose.yml -f compose.prod.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +66,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml run --rm --entrypoint "\
+sudo docker compose -f compose.yml -f compose.prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -77,4 +77,4 @@ sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml run --rm --
 echo
 
 echo "### Reloading nginx ..."
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml exec nginx nginx -s reload
+sudo docker compose -f compose.yml -f compose.prod.yml exec nginx nginx -s reload
