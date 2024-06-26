f5ea3638f062175e083890be071c89361410e6f4
MooGoo <moogoo78@gmail.com>
2024-06-27 06:41:25 +0800

    add ppi nginx

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(taibol.naturedb.org)
+domains=(ppi.naturedb.org)
 rsa_key_size=4096
 data_path="../naturedb-volumes/certbot"
 email="" # Adding a valid address is strongly recommended
