e1d61e4cc6a87274c484f1075acd4879a6d57731
moogoo <moogoo78@gmail.com>
2024-06-27 11:17:36 +0800

    certbot taibol.biodiv

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(ppi.naturedb.org)
+domains=(taibol.biodiv.tw)
 rsa_key_size=4096
 data_path="../naturedb-volumes/certbot"
 email="" # Adding a valid address is strongly recommended
